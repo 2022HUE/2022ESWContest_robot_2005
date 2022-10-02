c1715dd93494e1d182fead01631ea0bcee721713
Shimhyerin <nier8702@kookmin.ac.kr>
2022-10-02 18:51:52 +0900

    Chore: Convert cv2 to cv

--- Sensor/arrow.py
+++ Sensor/arrow.py
@@ -1,6 +1,5 @@
 import sys
 import cv2 as cv
-import cv2
 import numpy as np
 
 from Motion import Motion
@@ -8,23 +7,23 @@ motion = Motion()
 
 cnt_arrow = 0
 def get_arrow_info(origin, arrow_image):
-    arrow_info_image = cv2.cvtColor(origin.copy(), cv2.COLOR_GRAY2BGR)
-    _, contours, hierarchy = cv2.findContours(arrow_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    arrow_info_image = cv.cvtColor(origin.copy(), cv.COLOR_GRAY2BGR)
+    _, contours, hierarchy = cv.findContours(arrow_image, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
     
     if hierarchy is not None:
         global cnt_arrow
         for cnt in contours:
-            peri = cv2.arcLength(cnt, True)
-            approx = cv2.approxPolyDP(cnt, peri * 0.02, True)
+            peri = cv.arcLength(cnt, True)
+            approx = cv.approxPolyDP(cnt, peri * 0.02, True)
 
             # cv2.drawContours(arrow_info_image, [approx], 0, (0, 255, 0), 3)
-            area = cv2.contourArea(cnt)
+            area = cv.contourArea(cnt)
             vertices = len(approx) # 꼭짓점 개수
             # area 값 조절로 인식 조절
             if area > 30000 and vertices == 7:
                 print(cnt_arrow)
-                cv2.drawContours(origin, [approx], 0, (0, 255, 0), 3)
-                cv2.drawContours(arrow_info_image, [approx], 0, (0, 255, 0), 3)
+                cv.drawContours(origin, [approx], 0, (0, 255, 0), 3)
+                cv.drawContours(arrow_info_image, [approx], 0, (0, 255, 0), 3)
 
                 x_loc = []
                 for i in range(len(approx)):
@@ -72,31 +71,31 @@ while True:
         break
 
 
-    # img = cv2.resize(img, (640, 480))
-    # _, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV)
+    # img = cv.resize(img, (640, 480))
+    # _, img = cv.threshold(img, 0, 255, cv.THRESH_BINARY_INV)
     # origin = arrow[i]
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     img_copy = img.copy()
     
-    blur = cv2.GaussianBlur(img, (7, 7), 1)
+    blur = cv.GaussianBlur(img, (7, 7), 1)
     val = 10
     # arr = np.full(blur.shape, (val, val, val), np.uint8)
     # add = cv2.add(blur, arr)
     alpha = 1.0
     dst = np.clip((1+alpha)*blur - 128*alpha, 0, 255).astype(np.uint8)
-    _, img = cv2.threshold(dst, 0, 255, cv2.THRESH_BINARY_INV)
+    _, img = cv.threshold(dst, 0, 255, cv.THRESH_BINARY_INV)
 
     # arrow_image = get_filter_arrow_image(img)
     arrow_info_image, ret_arrow = get_arrow_info(img_copy, img)
     if (ret_arrow != None):
 
-        cv2.putText(img_copy, 'origin img', (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
-        cv2.putText(arrow_info_image, 'arrow: '+ret_arrow, (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
+        cv.putText(img_copy, 'origin img', (20, 30), cv.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2, cv.LINE_AA)
+        cv.putText(arrow_info_image, 'arrow: '+ret_arrow, (20, 30), cv.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2, cv.LINE_AA)
 
-        img_copy = cv2.cvtColor(img_copy, cv2.COLOR_GRAY2BGR)
-        ret = cv2.hconcat([img_copy, arrow_info_image])
-        cv2.imshow("ret", ret)
-        # cv2.imshow("ret_", img) # 마스킹 바이너리 이미지
+        img_copy = cv.cvtColor(img_copy, cv.COLOR_GRAY2BGR)
+        ret = cv.hconcat([img_copy, arrow_info_image])
+        cv.imshow("ret", ret)
+        # cv.imshow("ret_", img) # 마스킹 바이너리 이미지
     # else: print('################################################')
 
     # print(f"img[]: {ret_arrow}",)
--- Sensor/direction.py
+++ Sensor/direction.py
@@ -1,8 +1,5 @@
-import cv2
+import cv2 as cv
 import sys
-from cv2 import imshow
-from cv2 import cvtColor
-from cv2 import LINE_AA
 import numpy as np
 
 from Motion import Motion
@@ -10,7 +7,7 @@ motion = Motion()
 
 cnt_text = 0
 # cap = cv2.VideoCapture('./src/S.h264')
-cap = cv2.VideoCapture('./0925/entrance/entr03-1.mp4')
+cap = cv.VideoCapture('./0925/entrance/entr03-1.mp4')
 # none_img = cv2.imread('src/none.jpg',cv2.IMREAD_GRAYSCALE)
 
 if not cap.isOpened():
@@ -19,16 +16,16 @@ if not cap.isOpened():
 
 
 def text_masking(text):
-  hsv = cv2.cvtColor(text, cv2.COLOR_BGR2HSV)
-  h, s, v = cv2.split(hsv)
+  hsv = cv.cvtColor(text, cv.COLOR_BGR2HSV)
+  h, s, v = cv.split(hsv)
   
   # 폰트 이미지와 유사하게 만들기 위해 inverse해줌
-  ret_s, th_s = cv2.threshold(s, 120, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-  ret_v, th_v = cv2.threshold(v, 100, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-  # _, th_s = cv2.threshold(s, 120, 255, cv2.THRESH_BINARY)
-  # _, th_v = cv2.threshold(v, 100, 255, cv2.THRESH_BINARY_INV)
-  text_mask = cv2.bitwise_and(th_s, th_v)
-  # text_dst = cv2.bitwise_and(img_crop, img_crop, mask = text_mask) # remove background
+  ret_s, th_s = cv.threshold(s, 120, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
+  ret_v, th_v = cv.threshold(v, 100, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+  # _, th_s = cv.threshold(s, 120, 255, cv2.THRESH_BINARY)
+  # _, th_v = cv.threshold(v, 100, 255, cv2.THRESH_BINARY_INV)
+  text_mask = cv.bitwise_and(th_s, th_v)
+  # text_dst = cv.bitwise_and(img_crop, img_crop, mask = text_mask) # remove background
 
   return text_mask
 
@@ -45,9 +42,9 @@ def match_sam(sam_l, tar):
     padding_bottom, padding_right = 0, 0 # init
 
     if h > target_h or w > target_w:
-      interp = cv2.INTER_AREA
+      interp = cv.INTER_AREA
     else:
-      interp = cv2.INTER_CUBIC
+      interp = cv.INTER_CUBIC
     
     w = target_w
     h = round(w / ratio)
@@ -58,14 +55,14 @@ def match_sam(sam_l, tar):
     padding_bottom = abs(target_h - h)
     padding_right = abs(target_w - w)
 
-    scaled_img = cv2.resize(sample, (w, h), interpolation=interp)
-    padding_img = cv2.copyMakeBorder(scaled_img, padding_bottom//2, padding_bottom//2, padding_right//2, padding_right//2, 
-                                     borderType=cv2.BORDER_CONSTANT, value=[255,255,255])
+    scaled_img = cv.resize(sample, (w, h), interpolation=interp)
+    padding_img = cv.copyMakeBorder(scaled_img, padding_bottom//2, padding_bottom//2, padding_right//2, padding_right//2, 
+                                     borderType=cv.BORDER_CONSTANT, value=[255,255,255])
 
     # match = min(match, cv2.matchShapes(padding_img, tar, cv2.CONTOURS_MATCH_I3, 0))
-    ms = cv2.matchShapes(padding_img, tar, cv2.CONTOURS_MATCH_I3, 0)
-    mt = cv2.matchTemplate(padding_img, tar, cv2.TM_CCOEFF_NORMED)
-    _, maxv, _, _ = cv2.minMaxLoc(mt)
+    ms = cv.matchShapes(padding_img, tar, cv.CONTOURS_MATCH_I3, 0)
+    mt = cv.matchTemplate(padding_img, tar, cv.TM_CCOEFF_NORMED)
+    _, maxv, _, _ = cv.minMaxLoc(mt)
 
     if ms_score > ms:
       ms_score = ms
@@ -91,7 +88,7 @@ def matching(sam, tar, params):
   sample_img = max(match_list, key=lambda x: x[1][1])[1][2]
   ret_match_val = round(min(match_list, key=lambda x: x[1])[1][0], 4)
 
-  cv2.putText(sample_img,'[sample img] '+ret_mt, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255),1, cv2.LINE_AA)
+  cv.putText(sample_img,'[sample img] '+ret_mt, (10, 20), cv.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255),1, cv.LINE_AA)
   
   # test print
   # print('sample', ret_match, ret_match_val)
@@ -99,7 +96,7 @@ def matching(sam, tar, params):
   else: return None, ret_mt, sample_img
 
 # font_img matching
-font_img = [cv2.imread(f'./data/sample/font_img/{x}.jpg', cv2.IMREAD_GRAYSCALE) for x in range(4)]
+font_img = [cv.imread(f'./data/sample/font_img/{x}.jpg', cv.IMREAD_GRAYSCALE) for x in range(4)]
 def match_font(font_img,tar, params, name):
   target_h, target_w = tar.shape
   match = 100
@@ -113,9 +110,9 @@ def match_font(font_img,tar, params, name):
     padding_bottom, padding_right = 0, 0 # init
 
     if h > target_h or w > target_w:
-      interp = cv2.INTER_AREA
+      interp = cv.INTER_AREA
     else:
-      interp = cv2.INTER_CUBIC
+      interp = cv.INTER_CUBIC
     
     w = target_w
     h = round(w / ratio)
@@ -126,13 +123,13 @@ def match_font(font_img,tar, params, name):
     padding_bottom = abs(target_h - h)
     padding_right = abs(target_w - w)
 
-    scaled_img = cv2.resize(sample, (w, h), interpolation=interp)
-    padding_img = cv2.copyMakeBorder(scaled_img, padding_bottom//2, padding_bottom//2, padding_right//2, padding_right//2, 
-                                     borderType=cv2.BORDER_CONSTANT, value=[255,255,255])
+    scaled_img = cv.resize(sample, (w, h), interpolation=interp)
+    padding_img = cv.copyMakeBorder(scaled_img, padding_bottom//2, padding_bottom//2, padding_right//2, padding_right//2, 
+                                     borderType=cv.BORDER_CONSTANT, value=[255,255,255])
 
-    tmp = cv2.matchShapes(padding_img, tar, cv2.CONTOURS_MATCH_I3, 0)
-    mt = cv2.matchTemplate(padding_img, tar, cv2.TM_CCOEFF_NORMED)
-    _, maxv, _, _ = cv2.minMaxLoc(mt)
+    tmp = cv.matchShapes(padding_img, tar, cv.CONTOURS_MATCH_I3, 0)
+    mt = cv.matchTemplate(padding_img, tar, cv.TM_CCOEFF_NORMED)
+    _, maxv, _, _ = cv.minMaxLoc(mt)
 
     if match > tmp:
       match = tmp
@@ -155,10 +152,10 @@ def match_font(font_img,tar, params, name):
   # if match < params: 
 
   def show(matching_img, ret_val):
-    # cv2.cvtColor(matching_img, cv2.COLOR_GRAY2BGR)
-    cv2.rectangle(matching_img, (10,10), (100,20), (255, 255, 255), 10)
-    cv2.rectangle(matching_img, (10,10), (100,25), (0, 0, 0), 1)
-    cv2.putText(matching_img,'[font img] '+ret_val, (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 0),1, cv2.LINE_AA)
+    # cv.cvtColor(matching_img, cv2.COLOR_GRAY2BGR)
+    cv.rectangle(matching_img, (10,10), (100,20), (255, 255, 255), 10)
+    cv.rectangle(matching_img, (10,10), (100,25), (0, 0, 0), 1)
+    cv.putText(matching_img,'[font img] '+ret_val, (20, 20), cv.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 0),1, cv.LINE_AA)
     return matching_img
 
   if matching_num == 0: 
@@ -179,20 +176,20 @@ def match_font(font_img,tar, params, name):
 
 
 # sample E
-sample_e = [cv2.imread(f'./data/sample/sam_e0{x}.png', cv2.IMREAD_GRAYSCALE) for x in range(1, 6)]
+sample_e = [cv.imread(f'./data/sample/sam_e0{x}.png', cv.IMREAD_GRAYSCALE) for x in range(1, 6)]
 # sample W
-sample_w = [cv2.imread(f'./data/sample/sam_w0{x}.png', cv2.IMREAD_GRAYSCALE) for x in range(1, 6)]
+sample_w = [cv.imread(f'./data/sample/sam_w0{x}.png', cv.IMREAD_GRAYSCALE) for x in range(1, 6)]
 # sample N
-sample_n = [cv2.imread(f'./data/sample/sam_n0{x}.png', cv2.IMREAD_GRAYSCALE) for x in range(1, 6)]
+sample_n = [cv.imread(f'./data/sample/sam_n0{x}.png', cv.IMREAD_GRAYSCALE) for x in range(1, 6)]
 # sample S
-sample_s = [cv2.imread(f'./data/sample/sam_s0{x}.png', cv2.IMREAD_GRAYSCALE) for x in range(1, 6)]
+sample_s = [cv.imread(f'./data/sample/sam_s0{x}.png', cv.IMREAD_GRAYSCALE) for x in range(1, 6)]
 
 sample_list = [sample_e, sample_w, sample_s, sample_n]
 
 
 while True:
   ret, img = cap.read()
-  # img = cv2.resize(img, (640, 480))
+  # img = cv.resize(img, (640, 480))
   if not ret:
     print('No ret') 
     break
@@ -200,78 +197,78 @@ while True:
 
 
   img_copy = img.copy()
-  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-  blur = cv2.GaussianBlur(gray, (7, 7), 0)
+  gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+  blur = cv.GaussianBlur(gray, (7, 7), 0)
   val = 0
-  add = cv2.add(blur, val)
+  add = cv.add(blur, val)
   alpha = 0.0
   dst = np.clip((1+alpha)*add - 128*alpha, 0, 255).astype(np.uint8)
 
-  ret,th = cv2.threshold(dst, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-  dst = cv2.bitwise_and(dst, dst, mask = th)
+  ret,th = cv.threshold(dst, 0, 255, cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
+  dst = cv.bitwise_and(dst, dst, mask = th)
 
 
-  kernel=cv2.getStructuringElement(cv2.MORPH_RECT,(1,1))
-  dst=cv2.dilate(dst,kernel,iterations=1)
+  kernel=cv.getStructuringElement(cv.MORPH_RECT,(1,1))
+  dst=cv.dilate(dst,kernel,iterations=1)
 
 
-  edges = cv2.Canny(th, 100, 200, apertureSize=3)
+  edges = cv.Canny(th, 100, 200, apertureSize=3)
 
   # lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 70, maxLineGap=50)
   
-  _, contours1, hierarchy1 = cv2.findContours(th,  cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-  _, contours2, hierarchy2 = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+  _, contours1, hierarchy1 = cv.findContours(th,  cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+  _, contours2, hierarchy2 = cv.findContours(edges, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 
   text_cont = []
   for pos in range(len(contours1)):
-    peri = cv2.arcLength(contours1[pos], True)
-    approx = cv2.approxPolyDP(contours1[pos], peri * 0.02, True)
+    peri = cv.arcLength(contours1[pos], True)
+    approx = cv.approxPolyDP(contours1[pos], peri * 0.02, True)
     points = len(approx)
     if peri > 900 and points == 4:
       text_cont.append(contours1[pos])
       # match = matching(cont_s01, contours1[pos])
       # match = matching(cont_n02, contours1[pos])
-      cv2.drawContours(img, [approx], 0, (0, 255, 255), 1)
+      cv.drawContours(img, [approx], 0, (0, 255, 255), 1)
 
 
   # print(text_cont)
   # for cnt in contours2:
-  #   peri = cv2.arcLength(contours2[pos], True)
-  #   approx = cv2.approxPolyDP(contours2[pos], peri * 0.02, True)
+  #   peri = cv.arcLength(contours2[pos], True)
+  #   approx = cv.approxPolyDP(contours2[pos], peri * 0.02, True)
   #   points = len(approx)
   #   # if peri > 600 and points == 4:
   #   # text_cont.append(contours2[pos])
-  #   cv2.drawContours(img, [approx], 0, (255, 0, 255), 1)
+  #   cv.drawContours(img, [approx], 0, (255, 0, 255), 1)
 
-  #   cv2.drawContours(img, [approx], 0, (255, 0, 255), 1)
+  #   cv.drawContours(img, [approx], 0, (255, 0, 255), 1)
 
 
   # for pos in range(len(contours1)):
-  #   area = cv2.contourArea(contours1[pos])
+  #   area = cv.contourArea(contours1[pos])
   #   if area > 20000:
   #       contour_pos.append(pos)
 
 
   contour_pos = []
   for pos in range(len(text_cont)):
-    area = cv2.contourArea(text_cont[pos])
+    area = cv.contourArea(text_cont[pos])
     if area > 20000:
       contour_pos.append(pos)
 
   ##########################################################################
   # text count
   if text_cont: 
-    x, y, w, h = cv2.boundingRect(text_cont[0])
+    x, y, w, h = cv.boundingRect(text_cont[0])
     img_crop = img_copy[y:y+h, x:x+h]
     # cv2.imshow('tmp', img_crop)
-    text_gray = cv2.cvtColor(img_crop, cv2.COLOR_BGR2GRAY)
+    text_gray = cv.cvtColor(img_crop, cv.COLOR_BGR2GRAY)
     text = img_crop.copy()
     cnt_text += 1
-    print('text_cont:', cnt_text)
+    print('text_cont:', cnt_text) # debuging
 
   else:
     text = img.copy()
-    text_gray = cv2.cvtColor(text, cv2.COLOR_BGR2GRAY)
+    text_gray = cv.cvtColor(text, cv.COLOR_BGR2GRAY)
     cnt_text = 0
   ##########################################################################
     
@@ -279,14 +276,14 @@ while True:
   img_crop = img_copy
   for pos in contour_pos:
       # print(pos)
-      x, y, w, h = cv2.boundingRect(text_cont[pos])
+      x, y, w, h = cv.boundingRect(text_cont[pos])
       # print('x, y, w, h:', x, y, w, h)
       img_crop = img_copy[y:y + h, x:x + w]
   
   if cnt_text > 0:
     text_mask = text_masking(text)
-    text_edges = cv2.Canny(text_mask, 50, 150, apertureSize=3)
-    lines = cv2.HoughLinesP(text_edges,1,np.pi/180,40, minLineLength=0, maxLineGap=80)
+    text_edges = cv.Canny(text_mask, 50, 150, apertureSize=3)
+    lines = cv.HoughLinesP(text_edges,1,np.pi/180,40, minLineLength=0, maxLineGap=80)
 
     ms_gray, mt_gray, gimg = matching(sample_list, text_gray, 0.001)
     ms_mask, mt_mask, mimg = matching(sample_list, text_mask, 1) # ???
@@ -303,31 +300,26 @@ while True:
  
   
 
-  if cv2.waitKey(1) & 0xFF == ord('q'):
+  if cv.waitKey(1) & 0xFF == ord('q'):
     break
   
   # 출력 디버깅
   sz = text_mask.shape
   # print(sz)
-  # h1 = cv2.hconcat([text_mask, text_gray])
+  # h1 = cv.hconcat([text_mask, text_gray])
   # if match_mask_font and sz[0] < 300: 
   #   # print(sz[0], sz[1])
-  #   gimg = cv2.resize(gimg, (sz[1], sz[0]))
-  #   # mmimg = cv2.resize(mmimg, sz)
+  #   gimg = cv.resize(gimg, (sz[1], sz[0]))
+  #   # mmimg = cv.resize(mmimg, sz)
   #   # print(gimg.shape, mmimg.shape, text_mask.shape, text_gray.shape)
-  #   h2 = cv2.hconcat([gimg, mmimg])
-  #   ret = cv2.vconcat([h1, h2])
-  #   cv2.imshow('ret', ret)
+  #   h2 = cv.hconcat([gimg, mmimg])
+  #   ret = cv.vconcat([h1, h2])
+  #   cv.imshow('ret', ret)
   # else:
-  #   # cv2.imshow('ret', none_img)
+  #   # cv.imshow('ret', none_img)
   #   pass
 
-  cv2.imshow('origin', img)
-  # cv2.imshow('d', edges)
-  # cv2.imshow('correct', dst)
-  # cv2.imshow('see', text_mask)
-  # cv2.imshow('t_edges', text_edges)
-  # cv2.imshow('text', text)
+  cv.imshow('origin', img)
 
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
