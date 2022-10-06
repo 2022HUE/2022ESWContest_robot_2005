f8d31f4646d2b9cd459a158f20cae8ea2f32786b
anjiwon319 <anjiwon319@kookmin.ac.kr>
2022-10-06 21:14:48 +0900

    Fix: 사용하는 변수 global로 정리 및 가우시안 추가

--- Sensor/is_danger.py
+++ Sensor/is_danger.py
@@ -1,13 +1,23 @@
 import cv2 as cv
 import numpy as np
 
-global DANGER_CONST, ROOM_S, ROOM_V
+# 가우시안 블러 안씀 !
+
+global DANGER_CONST, ROOM_S, ROOM_V, MORPH_kernel, GAUSSIAN_kernel
 DANGER_CONST = 10
+# 위험 지역 인식 용도 s(채도) 기준값
 ROOM_S = 170
+# 위험 지역 인식 용도 v(명도) 기준값
 ROOM_V = 80
+# morphology kernel 값
+MORPH_kernel = 3
+# 가우시안 블러 kernel 값
+GAUSSIAN_kernel = 1
+
+
 
 def mophorlogy(mask):
-    kernel = np.ones((3, 3), np.uint8)
+    kernel = np.ones((MORPH_kernel, MORPH_kernel), np.uint8)
     mask = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel)
     mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, kernel)
     return mask
@@ -17,7 +27,8 @@ def get_s_mask(src):
     h, s, v = cv.split(hsv)
     ret_s, s_bin = cv.threshold(s, ROOM_S, 255, cv.THRESH_BINARY)
     # morphology 연산으로 노이즈 제거
-    s_bin = mophorlogy(s_bin)
+    # s_bin = mophorlogy(s_bin)
+    cv.imshow('s_bin', s_bin)
 
     return s_bin
 
@@ -26,12 +37,13 @@ def get_v_mask(src):
     h, s, v = cv.split(hsv)
     ret_v, v_bin = cv.threshold(v, ROOM_V, 255, cv.THRESH_BINARY)
     # morphology 연산으로 노이즈 제거
-    v_bin = mophorlogy(v_bin)
-
+    # v_bin = mophorlogy(v_bin)
+    cv.imshow('v_bin', v_bin)
     return v_bin
 
 def is_danger(src):
     mask_AND = cv.bitwise_and(get_s_mask(src), get_v_mask(src))
+    mask_AND = mophorlogy(mask_AND)
     cv.imshow('mask_AND', mask_AND)
     # 계단일 때 채색 비율: 80~200, 위험지역일 때 비율: 0~10
     rate = np.count_nonzero(mask_AND) / (640 * 480)
@@ -40,7 +52,7 @@ def is_danger(src):
     return "DANGER" if rate <= DANGER_CONST else "STAIR"
 
 # cap = cv.VideoCapture("videos/danger/1002_19:36.h264")
-# cap = cv.VideoCapture("videos/stair/0925_18:19.h264")
+cap = cv.VideoCapture("videos/stair/0925_18:19.h264")
 
 # while cap.isOpened():
 #     _, src = cap.read()
@@ -49,40 +61,22 @@ def is_danger(src):
 #         print("ret is false")
 #         break
 #     img_hsv = cv.cvtColor(src, cv.COLOR_BGR2HSV)
-#     # s_bin = get_s_mask(img_hsv)
-#     # h, s, v = cv.split(img_hsv)
-#     #
-#     #
-#     # cv.imshow('s_bin', s_bin)
-#     # # cv.imshow('img_s', img_s)
+#     blur = cv.GaussianBlur(src, (GAUSSIAN_kernel, GAUSSIAN_kernel), 1)
 #     cv.imshow('src', src)
 #
-#     # h, s, v = cv.split(img_hsv)
-#     # ret_v, v_bin = cv.threshold(v, 80, 255, cv.THRESH_BINARY)
-#     #
-#     # cv.imshow('v_bin', v_bin)
-#     #
-#     # mask_AND = cv.bitwise_and(s_bin, v_bin)
-#     # cv.imshow('mask_AND', mask_AND)
-#     #
-#     # # 계단일 때 채색 비율: 80~200, 위험지역일 때 비율: 0~10
-#     # rate = np.count_nonzero(mask_AND) / (640 * 480)
-#     # rate = int(rate * 1000)
-#     # img_s = cv.bitwise_and(src, src, mask=s_bin)
-#     # print(rate)
-#     print(is_danger(src))
+#     print(is_danger(blur))
 #
 #     if cv.waitKey(10) & 0xFF == ord('q'):
 #         break
 #
 # cap.release()
 
-'''
------------debug-----------------
 
-src = cv.imread('danger_room.png')
+# -----------debug-----------------
+
+src = cv.imread('src/danger/danger_room_0.png')
 
 cv.imshow('src', src)
-print(is_danger(src))
+blur = cv.GaussianBlur(src, (GAUSSIAN_kernel, GAUSSIAN_kernel), 1)
+print(is_danger(blur))
 cv.waitKey(0)
-'''
