87ee949a0a72f234aacc21359e049ec7b340c46f
anjiwon319 <anjiwon319@kookmin.ac.kr>
2022-10-30 22:56:10 +0900

    Feat: 알파벳 색상 마스크 hsv 값 추가

--- Sensor/Setting.py
+++ Sensor/Setting.py
@@ -15,10 +15,16 @@ setting.YELLOW_DATA = [[20, 100, 100], [35, 255, 255]]
 
 ########### DANGER DETECTION ###########
 
-# 파란색 색상 마스크 lower, upper의 [h, s, v] 값
+# 장애물 파란색 색상 마스크 lower, upper의 [h, s, v] 값
 setting.DANGER_H_BLUE = [[82, 87, 30], [130, 255, 120]]
-# 빨간색 색상 마스크 lower, upper의 [h, s, v] 값
+# 장애물 빨간색 색상 마스크 lower, upper의 [h, s, v] 값
 setting.DANGER_H_RED = [[167, 77, 30], [180, 255, 189]] # 실제로 hue값 가져왔을 때 167 까지 내려갔음 167 ~ 5
+
+# 알파벳 파란색 색상 마스크 lower, upper의 [h, s, v] 값
+setting.ALPHABET_BLUE = [[82, 87, 30], [130, 255, 120]]
+# 알파벳 빨간색 색상 마스크 lower, upper의 [h, s, v] 값
+setting.ALPHABET_RED = [[167, 77, 30], [180, 255, 189]]
+
 # 위험/계단 지역 판단하는 비율의 기준
 setting.DANGER_RATE = 10
 # 위험 지역 인식 용도 s(채도) 기준값
