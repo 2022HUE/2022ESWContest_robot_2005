c6c934e02bda8f0e1918884f3a40e1575af45938
Shimhyerin <nier8702@kookmin.ac.kr>
2022-11-03 19:58:09 +0900

    Feat: Arrow Detection 기능 모듈화 및 연결

--- Sensor/Setting.py
+++ Sensor/Setting.py
@@ -57,3 +57,6 @@ setting.ARROW_BLUR = 7
 setting.ARROW_BRIGHT = 1.0
 setting.DIR_BLUR = 7
 setting.DIR_KERNEL = 1
+########### ENTRANCE PROCESSING ###########
+setting.ARROW_BLUR = 7
+setting.ARROW_BRIGHT = 1.0
