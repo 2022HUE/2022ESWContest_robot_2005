d49c3d2b9eb0a0a5f3579e7c0e7b4d30b081d0f4
Shimhyerin <nier8702@kookmin.ac.kr>
2022-11-06 01:51:52 +0900

    Chore: 충돌 해결

--- Sensor/imageProcessor.py
+++ Sensor/imageProcessor.py
@@ -6,13 +6,17 @@ import platform
 from imutils.video import WebcamVideoStream
 from imutils.video import FileVideoStream
 from imutils.video import FPS
-
+  
 if __name__ == "__main__":
     from Line import Line
+    from Arrow import Arrow
+    from Direction import Direction
     from Setting import setting, LineColor
 
 else:
     from Sensor.Line import Line
+    from Sensor.Arrow import Arrow
+    from Sensor.Direction import Direction
     from Setting import setting, LineColor
 print(setting.YELLOW_DATA[0], setting.YELLOW_DATA[1])
 
@@ -22,8 +26,7 @@ class ImageProccessor:
         if video and os.path.exists(video):
             self._cam = FileVideoStream(path=video).start()
         else:
-            # print('s')
-            if platform.system() == "Linux": # Linux
+            if platform.system() == "Linux":
                 self._cam = WebcamVideoStream(src=-1).start()
             else:
                 self._cam = WebcamVideoStream(src=0).start()
@@ -52,15 +55,15 @@ class ImageProccessor:
     
 
     ########### 기본 공용 함수 ###########
-    def blur(self, img):
-        return cv.GaussianBlur(img, (7, 7), 1)
+    def blur(self, img, val):
+        return cv.GaussianBlur(img, (val, val), 1)
     def light(self, img, val): # 밝기
         arr = np.full(img.shape, (val, val, val), np.uint8)
         return cv.add(img, arr)
     def bright(self, img, alpha): # 명도
         return np.clip((1+alpha)*img - 128*alpha, 0, 255).astype(np.uint8)
-    def correction(self, img):
-        img = self.blur(img)
+    def correction(self, img, val):
+        img = self.blur(img, val)
         img = self.light(img, 0)
         img = self.bright(img, 0)
         return img
@@ -85,16 +88,13 @@ class ImageProccessor:
 
     
     ########### LINE DETECTION ###########
-    def line_detection(self, show):
+    # 라인이 수평선인지 수직선인지 return해줌
+    def is_line_horizon_vertical(self, show):
         img = self.get_img()
         origin = img.copy()
 
-
-        # height, width = img.shape[:2]
-        
-        img = self.correction(img)
+        img = self.correction(img, 7)
         hsv = self.hsv_mask(img)
-
         line_mask = Line.yellow_mask(self, hsv, setting.YELLOW_DATA)
         line_mask = self.HSV2BGR(line_mask)
         line_gray = self.RGB2GRAY(line_mask)
@@ -110,37 +110,143 @@ class ImageProccessor:
             left_line_arr, right_line_arr = Line.slope_filter(self, line_arr)
             left_line, right_line = Line.find_fitline(self, origin, left_line_arr), Line.find_fitline(self, origin, right_line_arr)
             
-            # draw
-            if left_line != 'failed_to_find_line' and Line.slope_cal(self, left_line):
+            # init
+            horizon = False
+            vertical = False
+
+            # check line "Horizontal or Vericality"
+            if left_line and Line.slope_cal(self, left_line):
                 if Line.slope_cal(self, left_line) < 10:
-                    # print('수평선!입니다!')
-                    Line.draw_fitline(self, origin, left_line, [0, 255, 0])
+                    # print('수평선!입니다!') # Debug
+                    # Line.draw_fitline(self, origin, left_line, [0, 255, 0]) # Debug
+                    horizon = True
                 elif 85 < Line.slope_cal(self, left_line) < 95:
-                    # print('수직선!입니다!')
-                    Line.draw_fitline(self, origin, left_line, [0, 255, 255])
-            if right_line != 'failed_to_find_line' and Line.slope_cal(self, right_line):
+                    # print('수직선!입니다!') # Debug
+                    # Line.draw_fitline(self, origin, left_line, [0, 255, 255]) # Debug
+                    vertical = True
+
+            if right_line and Line.slope_cal(self, right_line):
                 if Line.slope_cal(self, right_line) < 10:
-                    # print('수평선!입니다!')
-                    Line.draw_fitline(self, origin, right_line, [0, 255, 0])
+                    # print('수평선!입니다!') # Debug
+                    # Line.draw_fitline(self, origin, right_line, [0, 255, 0]) # Debug
+                    horizon = True
                 elif 85 < Line.slope_cal(self, right_line) < 95:
-                    # print('수직선!입니다!')
-                    Line.draw_fitline(self, origin, right_line, [0, 255, 255])
+                    # print('수직선!입니다!') # Debug
+                    # Line.draw_fitline(self, origin, right_line, [0, 255, 255]) # Debug
+                    vertical = True
+
+            ########### [Option] Show ##########
             if show:
-                cv.imshow("imageProcessor-get_img", origin)
+                cv.imshow("show", origin)
                 cv.waitKey(1) & 0xFF == ord('q')
+            ####################################
+
+            if horizon and vertical: return "BOTH" # 수직, 수평선 둘 다 인식
+            elif horizon: return "HORIZON" # 수직선만 인식
+            elif vertical: return "VERTICAL" # 수평선만 인식
+            else: return False
+
+        else: # 라인 자체를 인식 못할 경우 False 리턴
+            return False
+        
+
+        
+    ########### ENTRANCE PROCESSING ###########
+    # 화살표 방향 인식 후 리턴
+    def get_arrow(self, show):
+        img = self.get_img()
+        origin = img.copy()
+
+        img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        img = self.blur(img, setting.ARROW_BLUR)
+        img = self.bright(img, setting.ARROW_BRIGHT)
+        _, img = cv.threshold(img, 0, 255, cv.THRESH_BINARY_INV)
+        ret_arrow = Arrow.get_arrow_info(self, img)
+        if ret_arrow: print(ret_arrow) # Debug: print arrow
+
+        ########### [Option] Show ##########
+        if show:
+            cv.imshow("show", origin)
+            cv.waitKey(1) & 0xFF == ord('q')
+        ####################################
+
+        return ret_arrow
     
-    ########### MAP DIRECTION ###########
-    def get_map_direction(self):
-        pass
+    # 방위 글자 인식 후 방위 리턴
+    def get_direction(self, show):
+        img = self.get_img()
+        origin = img.copy()
+        dir = Direction
 
-    def get_map_arrow(self):
-        pass
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        dst = self.blur(gray, setting.DIR_BLUR)
 
-    ########### STAIR DIRECTION ###########
+        _,th = cv.threshold(dst, 0, 255, cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
+        dst = cv.bitwise_and(img, img, mask = th)
 
+        kernel=cv.getStructuringElement(cv.MORPH_RECT,(1,1))
+        dst=cv.dilate(dst,kernel,iterations=1)
+        
+        contours, hierarchy = cv.findContours(th,  cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+
+        roi_contour = []
+        for pos in range(len(contours)):
+            peri = cv.arcLength(contours[pos], True)
+            approx = cv.approxPolyDP(contours[pos], peri * 0.02, True)
+            points = len(approx)
+            if peri > 900 and points == 4:
+                roi_contour.append(contours[pos])
+                # cv.drawContours(img, [approx], 0, (0, 255, 255), 1) # Debug: Drawing Contours
+
+        roi_contour_pos = []
+        for pos in range(len(roi_contour)):
+            area = cv.contourArea(roi_contour[pos])
+            if area > 20000:
+                roi_contour_pos.append(pos)
+
+        if roi_contour: 
+            x, y, w, h = cv.boundingRect(roi_contour[0])
+            img_crop = origin[y:y+h, x:x+h]
+            text_gray = cv.cvtColor(img_crop, cv.COLOR_BGR2GRAY)
+            # text = img_crop.copy()
+
+            '''
+            [Issue]
+            현재 mt_gray(gray처리된 roi 부분을 matchTemplate 비교)값을 대표로 리턴함.
+            mt_gray, mt_mask가 정확도가 가장 높으며 두 값은 항상 유사한 결과를 가짐.
+            font 이미지와 비교한 2가지 값도 정확도가 낮지는 않으나, 가끔 로봇의 고개 각도에 따라 튀는 값이 나올 때가 있음
+            '''
+            mt_gray = Direction.matching(dir, Direction.sample_list, text_gray, 0.001) # 1. matchTemplate - Gray Scale
+            # mt_mask = dir.matching(dir.sample_list, text_mask, 1) # 2. matchTemplate - Masking
+            # text_mask = dir.text_masking(text)
+            # match_mask_font = dir.match_font(dir.font_img, text_mask) # 3. font <-> masking
+            # match_gray_font = dir.match_font(dir.font_img, text_gray) # 4. font <-> gray scale
+            
+            # print('match: ', mt_gray, mt_mask, match_gray_font, match_mask_font) # Debug: printing
+
+            ########### [Option] Show ##########
+            if show:
+                cv.imshow("show", origin)
+                # cv.imshow("show", img_crop)
+                cv.waitKey(1) & 0xFF == ord('q')
+            ####################################
+
+            print(mt_gray) # Debug: print return value
+            return mt_gray
+        else: # False
+            return ''
 
 if __name__ == "__main__":
-    img_processor = ImageProccessor(video="src/line/1003_line2.mp4")
+    ### Debug Path List ###
+    # entrance
+    arrow_path01 = "src/entrance/entr03-1.mp4"
+    arrow_path02 = "src/entrance/1027_23:14.h264"
+    # line
+    line_path01 = "src/line/1003_line2.mp4"
+    img_processor = ImageProccessor(video=line_path01)
     
+    ### Debug Run ###
     while True:
+        # img_processor.get_arrow(show=True)
+        # img_processor.get_direction(show=True)
         img_processor.line_detection(show=True)
