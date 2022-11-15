7b7ca59518c3d921ece8555e1a17484f1ff28bd5
anjiwon319 <anjiwon319@kookmin.ac.kr>
2022-11-15 20:55:33 +0900

    Feat: WALK_TO_MILKBOX, OUT_OF_DANGER 부분 로직 구현 완료 및 필요한 ACT 수정

--- Core/Misson.py
+++ Core/Misson.py
@@ -19,8 +19,8 @@ class Act(Enum):
     DETECT_ALPHABET = auto()  # 방이름 감지
     WALK_TO_MILKBOX = auto()  # 장애물 찾기
     OUT_OF_DANGER = auto()  # 위험지역 밖으로 장애물 옮기기
-    SPEAK_ALPHABET = auto()  # 알파벳 글자 말하기 (방 코너점까지 이동 후 말하기)
-    REGRAB_MILKBOX = auto() # 떨어진 장애물 다시 잡기 -> WALK_TO_MILKBO로 충분할 것 같은데 일단 넣음
+    REGRAB_MILKBOX = auto() # 떨어진 장애물 다시 잡기 -> WALK_TO_MILKBOX로 충분할 것 같아서 일단 안씀
+    KICK_MILKBOX = auto() # 자꾸 장애물을 떨어트릴 경우 이 방법 사용 (발로 차거나 치우기 동작 수행)
     EXIT = auto()  # 공통
 
 
@@ -166,14 +166,32 @@ class MissonDanger:
     limits: int = 3
     alphabet_color: str
     alphabet_name: str
-    milkbox_idx: int
+    milkbox_pos: int
+    head_angle: int = 70
+    holding: bool
+    first_milkbox_pos: int
 
     def init_robo(self, robo: Robo):
         self.robo = robo
 
     @classmethod
-    def set_milkbox_pos_to_7(self):
-        pass
+    def is_okay_grab_milkbox(self):
+        if self.head_angle == 70:
+            self.head_angle = 45
+            # motion : 고개 각도 45도로 설정
+
+            return False
+        elif self.head_angle == 45:
+            self.head_angle = 30
+            # motion : 고개 각도 30도로 설정
+
+            return False
+        else:
+            # motion : 장애물 잡기 동작 수행
+
+            # motion : 장애물 집고 앞으로 한번 걷기 동작 수행
+
+            return True
 
     @classmethod
     def go_robo(self):
@@ -192,7 +210,6 @@ class MissonDanger:
 
         elif act == act.DETECT_ALPHABET:
             # 방 알파벳 색상 인식 -> 보기 불편해서 함수로 빼는 게 좋을 듯
-
             # 색상 고정이면 이렇게 하고 싶은데 위험 지역 2개 돌 경우,
             # 이전 세팅에 저장되어있는 값 사용하게 되어 다른 색상을 집을 수 있는 문제 발생
             # if not cur.ALPHABET_COLOR:
@@ -224,23 +241,98 @@ class MissonDanger:
             self.act = Act.WALK_TO_MILKBOX
 
         elif act == act.WALK_TO_MILKBOX:
+            # 위치가 여기가 맞을 지 모르겠지만 일단 여기에 둠
+            self.first_milkbox_pos = self.robo._image_processor.get_milkbox_pos(cur.ALPHABET_COLOR)
             while True:
-                self.milkbox_idx = self.robo._image_processor.get_milkbox_pos(cur.ALPHABET_COLOR)
-                if self.milkbox_idx == 7:
+                self.milkbox_pos = self.robo._image_processor.get_milkbox_pos(cur.ALPHABET_COLOR)
+                # 9개 구역에 따라 다른 모션 수행
+                if self.milkbox_pos == 7:
+                    if self.is_okay_grab_milkbox():
+                        self.act = Act.OUT_OF_DANGER_OBJ
+                        break
+                elif self.milkbox_pos == 1 or self.milkbox_pos == 4:
+                    # motion : 앞으로 걷기 1번 수행
+                    pass
+                elif self.milkbox_pos == 0 or self.milkbox_pos == 3 or self.milkbox_pos == 6:
+                    # motion : 왼쪽으로 20도 회전 수행
+                    pass
+                elif self.milkbox_pos == 2 or self.milkbox_pos == 5 or self.milkbox_pos == 8:
+                    # motion : 오른쪽으로 20도 회전 수행
                     pass
 
-            # self.act = Act.OUT_OF_DANGER_OBJ
+                # milkbox_pos 를 가져오지 못한 경우
+                elif self.miss >= self.limits:
+                    # 계속 못찾으면 그냥 시민 대피 미션 포기 (실패할 경우 EXIT 버전 하나 더 만들어야할 듯)
+                    self.act = Act.EXIT
+                else:
+                    self.miss += 1
+                    return False
+
+            self.act = Act.OUT_OF_DANGER
+
+
         elif act == act.OUT_OF_DANGER:
             # 장애물을 들고 있는 채로 위험지역 밖을 벗어날 때까지 아래 과정 반복
-            while not self.robo._image_processor.is_out_of_black():
+            while True:
                 if not self.robo._image_processor.is_holding_milkbox():
+                    # motion : 장애물 내려놓기 동작 수행
+
                     self.act = Act.WALK_TO_MILKBOX
+                    return False
+                if self.robo._image_processor.is_out_of_black():
+                    # motion : 장애물 내려놓기 동작 수행
 
-            self.act = Act.SPEAKING_ALPHABET
+                    break
+                # 무한 루프 갇힐 경우에 대한 예외처리 미처리
+                else:
+                    if self.first_milkbox_pos == 0:
+                        # motion: 장애물 집고 왼쪽으로 45도 돌기 동작 수행
 
-        elif act == act.SPEAKING_ALPHABET:
-            # motion "위험지역 글자(cur.ALPHABET_NAME)" 음성 말하기
+                        # motion: 장애물 집고 앞으로 두 발자국 걷기 동작 수행
+                        pass
+                    elif self.first_milkbox_pos == 1:
+                        # motion: 장애물 집고 왼쪽으로 45도 돌기 동작 수행
+
+                        # motion: 장애물 집고 앞으로 두 발자국 걷기 동작 2번 수행
+                        pass
+                    elif self.first_milkbox_pos == 2:
+                        # motion: 장애물 집고 오쪽으로 45도 돌기 동작 수행
+
+                        # motion: 장애물 집고 앞으로 두 발자국 걷기 동작 2번 수행
+                        pass
+                    elif self.first_milkbox_pos == 3:
+                        # motion: 장애물 집고 왼쪽으로 60도 돌기 동작 수행
+
+                        # motion: 장애물 집고 앞으로 두 발자국 걷기 동작 2번 수행
+                        pass
+                    elif self.first_milkbox_pos == 4:
+                        # motion: 장애물 집고 왼쪽으로 45도 돌기 동작 수행
+
+                        # motion: 장애물 집고 앞으로 두 발자국 걷기 동작 2번 수행
+                        pass
+                    elif self.first_milkbox_pos == 5:
+                        # motion: 장애물 집고 오른쪽으로 60도 돌기 동작 수행
+
+                        # motion: 장애물 집고 앞으로 두 발자국 걷기 동작 2번 수행
+                        pass
+                    elif self.first_milkbox_pos == 6:
+                        # motion: 장애물 집고 왼쪽으로 60도 돌기 동작 수행
+
+                        # motion: 장애물 집고 앞으로 두 발자국 걷기 동작 수행
+                        pass
+                    elif self.first_milkbox_pos == 7:
+                        # motion: 장애물 집고 왼쪽으로 60도 돌기 동작 수행
+
+                        # motion: 장애물 집고 앞으로 두 발자국 걷기 동작 2번 수행
+                        pass
+                    elif self.first_milkbox_pos == 8:
+                        # motion: 장애물 집고 오른쪽으로 60도 돌기 동작 수행
+
+                        # motion: 장애물 집고 앞으로 두 발자국 걷기 동작 2번 수행
+                        pass
+
             self.act = Act.EXIT
+
 
         else:  # EXIT
             return True
