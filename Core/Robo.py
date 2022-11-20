4773e91703bb501fa590b413acec21cd651e81b5
anjiwon319 <anjiwon319@kookmin.ac.kr>
2022-11-20 18:31:34 +0900

    Feat: 로컬 컴퓨터에서 실행하기 위해 수정

--- Core/Robo.py
+++ Core/Robo.py
@@ -2,14 +2,16 @@
 from Sensor.ImageProcessor import ImageProccessor
 from Motion.Motion import Motion
 from Setting import Setting
+from Sensor.DataPath import DataPath
 
 class Robo:
     arrow: str = "LEFT"
     dis_arrow: str = "RIGHT"
     black_room_list = []
     alphabet_color: str
+    # _image_processor 이거 없어도 되는 거 맞쥬?
     _image_processor = ImageProccessor(video="") # Image Processor
 
-    def __init__(self, vpath=''):
+    def __init__(self, vpath='Sensor/src/danger/1110_22:29.h264'):
         self._image_processor = ImageProccessor(video=vpath) # Image Processor
-        self._motion = Motion() # Motion
+        # self._motion = Motion() # Motion
