2be10f995dd9a70e99ed9bb14dba98d473357b05
Gongchaeyeon <dnjftjd53@naver.com>
2022-11-21 20:07:07 +0900

    Chore: setting.arrow 사용 하지 않고 MissionStair에서 Robo.arrow 부여하도록 코드 수정

--- Core/Robo.py
+++ Core/Robo.py
@@ -13,6 +13,6 @@ class Robo:
     stair_level: int = 1
     # _image_processor = ImageProccessor(video="") # Image Processor
 
-    def __init__(self, vpath=''):
+    def __init__(self, vpath='Sensor/src/stair/1106_20:13.h264'):
         self._image_processor = ImageProccessor(video=vpath) # Image Processor
-        self._motion = Motion() # Motion
+        # self._motion = Motion() # Motion
